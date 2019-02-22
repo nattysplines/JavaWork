df5cf1efcc2e4d8a28d1966eab563e3a88b1e5f7
Carlo <carlodelosreyes07@gmail.com>
2019-02-22 11:26:25 -0800

    Modified adjlist version to have weights

--- Graphs.java
+++ Graphs.java
@@ -3,46 +3,61 @@ import java.util.LinkedList;
 public class Graphs {
     
     public static void main(String[] args) {
-        AdjListGraph graph = new AdjListGraph(5);
+
+        /*
+        *   Graph represented by below edges.
+        *   1--0  7--6
+        *   |  | /| /|
+        *   |  |/ |/ |
+        *   2  3--4--5
+        */
+
+        AdjListGraph graph = new AdjListGraph(8);
         graph.addEdge(0, 1);
-        graph.addEdge(0, 4);
+        graph.addEdge(0, 3);
         graph.addEdge(1, 2);
-        graph.addEdge(1, 3);
-        graph.addEdge(1, 4);
-        graph.addEdge(2, 3);
         graph.addEdge(3, 4);
-        
-        MatrixGraph mGraph = new MatrixGraph(5);
-        mGraph.addEdge(0, 1);
-        mGraph.addEdge(0, 4);
-        mGraph.addEdge(1, 2);
-        mGraph.addEdge(1, 3);
-        mGraph.addEdge(1, 4);
-        mGraph.addEdge(2, 3);
-        mGraph.addEdge(3, 4);
+        graph.addEdge(3, 7);
+        graph.addEdge(4, 5);
+        graph.addEdge(4, 6);
+        graph.addEdge(4, 7);
+        graph.addEdge(5, 6);
+        graph.addEdge(6, 7);
         
         System.out.println(graph);
-        System.out.println();
-        System.out.println(mGraph);
+    }
+}
+
+class Edge {
+    int nodeId;
+    int distance;
+
+    public Edge(int nodeId, int distance) {
+        this.nodeId = nodeId;
+        this.distance = distance;
     }
 }
 
 class AdjListGraph {
     private int v;
-    LinkedList<Integer>[] adjList;
+    LinkedList<Edge>[] adjList;
 
     public AdjListGraph(int v) {
         this.v = v;
         adjList = new LinkedList[v];
 
         for (int i = 0; i < adjList.length; ++i) {
-            adjList[i] = new LinkedList<Integer>();
+            adjList[i] = new LinkedList<Edge>();
         }
     }
 
     public void addEdge(int start, int end) {
-        adjList[start].add(end);
-        adjList[end].add(start);
+        adjList[start].add(new Edge(end, 1));
+        adjList[end].add(new Edge(start, 1));
+    }
+    public void addEdge(int start, int end, int distance) {
+        adjList[start].add(new Edge(end, distance));
+        adjList[end].add(new Edge(start, distance));
     }
 
     @Override
@@ -51,8 +66,8 @@ class AdjListGraph {
         sb.append("Adj List Version: \n");
         for (int i = 0; i < adjList.length; ++i) {
             sb.append(i);
-            for (Integer item : adjList[i]) {
-                sb.append(" -> " + item);
+            for (Edge item : adjList[i]) {
+                sb.append(" -> " + item.nodeId);
             }
             sb.append("\n");
         }
